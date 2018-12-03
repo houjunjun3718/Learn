ed659ff691e2d8125a16cd6476208f649c5737c0
候军军小黑 <15847703113@163.com>
2018-12-03 10:49:58 +0800

    添加了C++的分支

---
+++ CPP/myfirst.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: myfirst.cpp
+	> Author: 候军军
+	> Mail: 15847703113@163.com
+	> Created Time: 2018年12月02日 星期日 21时35分30秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+
+    return 0;
+}
+
+
